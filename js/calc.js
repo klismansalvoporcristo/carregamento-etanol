b1eafb9fab7ec623d409ecdbc607b9aecbc10a9c
Klisman Salvoporcristo <108096965+klismansalvoporcristo@users.noreply.github.com>
2023-11-24 17:48:15 -0300

    inplant sistem

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,7 @@
 function converter (num) {
-  var coranteMl = num*15
-  var coranteL= coranteMl/1000
+  var coranteMl = (num/1000)*15
+  var coranteL= (coranteMl/1000).toFixed(3)
+  document.getElementById("resM").innerText=(num/1000)+" Metros cubicos de etanol"
   document.getElementById("res").innerText=coranteMl+" Ml de corante"
   document.getElementById("resL").innerText=coranteL+" Litros de corante"
 }
